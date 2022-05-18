de6662ef372bc4e687aa385b1e38cccaa2a38c01
jaydeepparmar2244 <jaydeep2244.67@gmail.com>
2022-05-18 09:00:07 +0530

    view one book and student added

--- db/seeds/initial_data.js
+++ db/seeds/initial_data.js
@@ -9,8 +9,8 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('books').insert([
-        { id: 1, bookName: 'Power of Subconscious Mind', author:'Joseph Murphy',borrowDate:'2022-05-16',returnDate:'2022-05-23',borrowedBy:'1'},
-        { id: 2, bookName: 'Harry Potter', author:'Joanne Kathleen Rowling',borrowDate:'2022-05-17',returnDate:'2022-05-28',borrowedBy:'1'},
+        { id: 1, bookName: 'Power of Subconscious Mind', author:'Joseph Murphy',borrowDate:'2022-05-16',returnDate:'2022-05-23',borrowedBy:1},
+        { id: 2, bookName: 'Harry Potter', author:'Joanne Kathleen Rowling',borrowDate:'2022-05-17',returnDate:'2022-05-28',borrowedBy:2},
         { id: 3, bookName: 'Atomic Habits', author:'James Clear'}
       ]);
     });
--- index.js
+++ index.js
@@ -12,9 +12,26 @@ app.use(
 )
 
 app.get('/students', async function (req, res, next) {
-    const users = await DB('students').select(['id', 'firstName','lastName']);
-    return res.json(users);
-  });
+    const students = await DB('students').select(['id', 'firstName','lastName']);
+    return res.json(students);
+});
+
+app.get('/students/:id', async function (req, res, next) {
+    const id = req.params.id
+    const students = await DB('students').where('id',id).select(['id', 'firstName','lastName']);
+    return res.json(students);
+});
+
+app.get('/books', async function (req, res, next) {
+    const books = await DB('books').select(['id', 'bookName','author','borrowDate','returnDate','borrowedBy']);
+    return res.json(books);
+});
+
+app.get('/books/:id', async function (req, res, next) {
+    const id = req.params.id
+    const books = await DB('books').where('id',id).select(['id', 'bookName','author','borrowDate','returnDate','borrowedBy']);
+    return res.json(books);
+});
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
